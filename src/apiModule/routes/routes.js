0ba203d59dcb590cbaaddc53d345498e7252e952
eerbano <enzo.erbano@gmail.com>
2021-02-06 13:11:02 -0400

    Endpoint files length

--- src/apiModule/routes/routes.js
+++ src/apiModule/routes/routes.js
@@ -1,11 +1,11 @@
 const multer = require('multer');
 const express = require('express');
 const apiRouter = express.Router();
-const apiService = require('../services/ServicesInterface');
+const apiService = require('../services/servicesInterface');
 
 const uploadFolder = multer({
     dest: './res/newData',
-    limits: { fileSize: 5000000, files: 5 }, // ten files of five Mb each
+    limits: { fileSize: 5000000, files: 8 }, // ten files of five Mb each
 })
 // apiRouter.post('/', apiService.insertTransaction);
 // apiRouter.get('/:id', apiService.findTransactionById);
@@ -17,7 +17,7 @@ const uploadFolder = multer({
 // apiRouter.delete('/period/:yearMonth', apiService.removeAllTransactionsInPeriod);
 // apiRouter.post('/periods', apiService.findUniquePeriods);
 apiRouter.post('/classifySingleImage', uploadFolder.single('upload'), apiService.classifySingleImage)
-apiRouter.post('/classifyMultipleImages', uploadFolder.array('upload', 5), apiService.classifyMultipleImages)
+apiRouter.post('/classifyMultipleImages', uploadFolder.array('upload', 8), apiService.classifyMultipleImages)
 
 
 
