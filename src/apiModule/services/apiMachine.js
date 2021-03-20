aa73447b97af68cd1b8cb4d0810b81abb6f99b90
eerbano <enzo.erbano@gmail.com>
2021-03-20 00:17:41 -0400

    Refactored with helpers

--- src/apiModule/services/apiMachine.js
+++ src/apiModule/services/apiMachine.js
@@ -1,98 +1,75 @@
-const apiModel = require('../model/apiModel');
+const helpers = require('./helpers')
 
 const apiMachine = {}
 
 apiMachine.classifySingleImage = async (req, res) =>
 {
-    checkFileType(req, res);
-    const controllerModuleHandler = req.app.locals.controllerModuleHandler
-    const fileObject = newFileObject(req.file)
-    const mlObject = await controllerModuleHandler.singleImageClassification(fileObject)
-
-    if (mlObject.prediction)
+    if (helpers.checkFileType(req, res))
     {
-        res.send(mlObject);
+        const singleImageClassification = req.app.locals.controllerModuleHandler.singleImageClassification
+        // const dbModel = req.app.locals.controllerModuleHandler.dbModel
+
+        // try
+        // {
+        //     let newTransactionJSON = helpers.newTransaction({ description: "teste", value: 5, category: "", year: 2020, month: 5, day: 1, type: "expense" });
+
+        //     const transaction = new dbModel(newTransactionJSON);
+        //     await transaction.save();
+        // } catch (error)
+        // {
+        //     console.error(error)
+        // }
+
+        const fileObject = helpers.newFileObject(req.file)
+        const mlObject = await singleImageClassification(fileObject)
+
+        if (mlObject.prediction)
+        {
+            res.send(mlObject);
+        }
+        else
+        {
+            noPredictionResponse(res);
+        }
     }
     else
     {
-        noPredictionResponse(res);
+        notImageDataResponse()
     }
-};
+}
 
 apiMachine.classifyMultipleImages = async (req, res) =>
 {
-    const rejectedFiles = checkFileType(req, res);
-    const controllerModuleHandler = req.app.locals.controllerModuleHandler
+    const rejectedFiles = helpers.checkFileType(req, res);
 
-    const files = newFilesObjectArray(req.files)
-    const mlObjectArray = await controllerModuleHandler.multipleImageClassificationV(files)
-    //const mlObjectArray = await controllerModuleHandler.multipleImageClassificationMT1(files)
-
-    if (mlObjectArray.length)
+    if (rejectedFiles)
     {
-        res.send({ classified: mlObjectArray, rejected: rejectedFiles });
+        const multipleImageClassificationV = req.app.locals.controllerModuleHandler.multipleImageClassificationV
+        const dbModel = req.app.locals.controllerModuleHandler.dbModel
+
+        const files = helpers.newFilesObjectArray(req.files)
+        const mlObjectArray = await multipleImageClassificationV(files)
+        //const mlObjectArray = await multipleImageClassificationMT1(files)
+
+        if (mlObjectArray.length)
+        {
+            res.send({ classified: mlObjectArray, rejected: rejectedFiles });
+        }
+        else
+        {
+            noPredictionResponse(res);
+        }
     }
     else
     {
-        noPredictionResponse(res);
-    }
-};
-
-const newFileObject = (file) =>
-{
-    const fileObject = {
-        path: file.destination + "/",
-        name: file.filename,
-        originalName: file.originalname
-    }
-    return fileObject
-}
-
-const newFilesObjectArray = (files) =>
-{
-    const filesObject = []
-    for (let index = 0; index < files.length; index++) 
-    {
-        const fileElement = files[index];
-        filesObject.push(newFileObject(fileElement))
-    }
-    return filesObject
-}
-
-const checkFileType = (req, res) =>
-{
-    let rejectedFiles = []
-    if (req.file)
-    {
-        if (!req.file.originalname.match(/\.(png|jpg|jpeg)$/))
-        {
-            res.status(400).send({ message: 'Data that was sent is not an image', });
-        }
-    }
-    else if (req.files)
-    {
-        for (let index = 0; index < req.files.length; index++)
-        {
-            const fileElement = req.files[index];
-            if (!fileElement.originalname.match(/\.(png|jpg|jpeg)$/))
-            {
-                rejectedFiles.push(fileElement)
-                req.files.splice(index, 1);
-                index--
-            }
-        }
-
-        if (req.files.length == 0)
-        {
-            res.status(400).send({ message: 'Data that was sent is not an image', });
-        }
-        else 
-        {   // There are images to be processed yet
-            return rejectedFiles
-        }
+        notImageDataResponse()
     }
 }
 
+const notImageDataResponse = () =>
+{
+    res.status(400).send({ message: 'Data that was sent is not an image', });
+}
 
 const noPredictionResponse = (res) =>
 {
